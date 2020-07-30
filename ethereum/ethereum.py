d9f4657c2704d7d3824e3f54eab4a9fcbc13de51
ct <1768012206@qq.com>
2020-07-30 15:11:52 +0800

    以太坊1.02

--- ethereum/ethereum.py
+++ ethereum/ethereum.py
@@ -1,17 +1,19 @@
 from Crypto.Util.number import *
-
+from web3 import exceptions
 from utils import utils
-from solc import compile_standard
 import json
-import pprint
 
-
+"""
+    本类基本没有做异常处理...
+    尤其是设计到和合约交互的部分，可能会由于gas limit等问题导致交互不成功，可根据返回的tx_receipt来查询trans具体状态
+    前三个函数暂时用不到，主要使用和合约交互的后面几个函数就可以了
+"""
 class Ethereum:
     def __init__(self):
         # 获取w3, my
         self.w3, self.my = utils.get_context()
         self.w3.eth.defaultAccount = self.my.address
-        self.contract_instances = {}
+        self.contract_instance = None
 
     # 签发包含msg的trans到区块链
     def send_to_ether(self, msg):
@@ -35,78 +37,12 @@ class Ethereum:
         # data_in_int = int(data, 16)
         # return long_to_bytes(data_in_int).decode('utf-8')
 
-    # 获取合约$name，需要在deploy_contract之后调用，否则返回None
-    def get_contract_by_name(self, name):
-        return self.contract_instances.get(name)
-
-    # 获取所有合约
-    def get_all_contract(self):
-        return self.contract_instances
-
-    """
-    source = contract Greeter {
-                string public greeting;
-    
-                function Greeter() {
-                    greeting = 'Hello';
-                }
-    
-                function setGreeting(string _greeting) public {
-                    greeting = _greeting;
-                }
-    
-                function greet() constant returns (string) {
-                    return greeting;
-                }
-            }
-        #sol should like this
-        sol = {
-            "name": "Greeter.sol",
-            "body": source
-        }
-    """
-
-    def deploy_contract(self, sol, sol_name):
-        sol_name = "Greeter"
-
-        source = """contract Greeter {
-                        string public greeting;
-
-                        function Greeter() {
-                            greeting = 'Hello';
-                        }
-
-                        function setGreeting(string _greeting) public {
-                            greeting = _greeting;
-                        }
-
-                        function greet() constant returns (string) {
-                            return greeting;
-                        }
-                    }
-                """
+    def deploy_contract(self):
+        sol_name = "hackfs"
         # sol should like this
-        sol = {
-            "name": "Greeter.sol",
-            "body": source
-        }
-        compiled_sol = compile_standard({
-            "language": "Solidity",
-            "sources": {
-                sol.get("name"): {
-                    "content": sol.get("body")
-                }
-            },
-            "settings": {
-                "outputSelection": {
-                    "*": {
-                        "*": ["metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
-                    }
-                }
-            }
-        })
-        bytecode = compiled_sol['contracts'][sol.get("name")][sol_name]['evm']['bytecode']['object']
-        abi = json.loads(compiled_sol['contracts'][sol.get("name")][sol_name]['metadata'])['output']['abi']
+        compiled_sol = utils.get_compiled_contract()
+        bytecode = compiled_sol['contracts']["eth.sol"][sol_name]['evm']['bytecode']['object']
+        abi = json.loads(compiled_sol['contracts']["eth.sol"][sol_name]['metadata'])['output']['abi']
         contract = self.w3.eth.contract(abi=abi, bytecode=bytecode)
         transaction = {
             'gasPrice': self.w3.eth.gasPrice,
@@ -118,18 +54,83 @@ class Ethereum:
         tx_hash = self.w3.eth.sendRawTransaction(transaction.rawTransaction)
 
         tx_receipt = self.w3.eth.waitForTransactionReceipt(tx_hash)
-        self.contract_instances[sol_name] = self.w3.eth.contract(
+        self.contract_instance = self.w3.eth.contract(
             address=tx_receipt.contractAddress,
             abi=abi
         )
         # return self.contract_instance
 
+    # 必须在和合约交互之前调用
+    def get_contract_instance(self, contractAddress):
+        contract_instance = self.w3.eth.contract(
+            address=contractAddress,
+            abi=json.loads(utils.get_compiled_contract()['contracts']["eth.sol"]["hackfs"]['metadata'])['output']['abi']
+        )
+        self.contract_instance = contract_instance
 
-# print(send_to_ether("我爱张华", "你的key").hex())
+    # 接下来的函数和合约函数一一对应，文档可以参考石墨
+    def getHash(self, uuid):
+        try:
+            data = self.contract_instance.functions.getHash(uuid).call()
+        except exceptions.BadFunctionCallOutput:
+            return "no such uuid"
+        return data
 
+    """
+        下列函数的运行时间不稳定，最好使用异步或者回调来确保前台响应效率...
+    """
+
+    def newHash(self, value):
+        cool_function = self.contract_instance.functions.newHash(value)
+        trans = cool_function.buildTransaction(
+            {'nonce': self.w3.eth.getTransactionCount(self.my.address), 'gas': 1000000, "from": self.my.address,
+             'gasPrice': self.w3.eth.gasPrice})
+        signed = self.my.signTransaction(trans)
+        tx_hash = self.w3.eth.sendRawTransaction(signed.rawTransaction)
+        tx_receipt = self.w3.eth.waitForTransactionReceipt(tx_hash)
+        # 返回值只是为了确认成功
+        return tx_receipt
+
+    def updateHash(self, oldHashValue, newHashValue):
+        cool_function = self.contract_instance.functions.updateHash(oldHashValue, newHashValue)
+        trans = cool_function.buildTransaction(
+            {'nonce': self.w3.eth.getTransactionCount(self.my.address), 'gas': 1000000, "from": self.my.address,
+             'gasPrice': self.w3.eth.gasPrice})
+        signed = self.my.signTransaction(trans)
+        tx_hash = self.w3.eth.sendRawTransaction(signed.rawTransaction)
+        tx_receipt = self.w3.eth.waitForTransactionReceipt(tx_hash)
+        return tx_receipt
+
+    def newRelation(self, uuid, hashValue):
+        cool_function = self.contract_instance.functions.newRelation(uuid, hashValue)
+        trans = cool_function.buildTransaction(
+            {'nonce': self.w3.eth.getTransactionCount(self.my.address), 'gas': 1000000, "from": self.my.address,
+             'gasPrice': self.w3.eth.gasPrice})
+        signed = self.my.signTransaction(trans)
+        tx_hash = self.w3.eth.sendRawTransaction(signed.rawTransaction)
+        tx_receipt = self.w3.eth.waitForTransactionReceipt(tx_hash)
+
+        return tx_receipt
+
+    # 备用
+    def call_what_you_want(self, name):
+        contract_func = self.contract_instance.functions[name]
+        twentyone = contract_func("params").call()  # 是否有参数取决于合约，有参数提交的需要创建真实trans
+        return twentyone
+
+
+"""
+下面是部分测试用例
+"""
+
+# 实例化对象
 ethereum = Ethereum()
-ins = ethereum.deploy_contract(1, 2)
-print(ethereum.contract_instance)
+# 初始化合约对象
+ethereum.get_contract_instance("0x8d592ad6DA67C3FdDe95a3cD8c33441F29C39836")
 
-# print(ins.functions.greet().call())
-# print(ethereum.get_from_ether("0x1f2c6bf11571f5609004e7fb316879261e4ace728e88923fb23d8ec31be1bff8"))
+# 合约存入新哈希
+# print(ethereum.newHash("0x8d592ad6DA67C3FdDe95a3cD8c33441F29C39836"))
+# 建立新关系
+# print(ethereum.newRelation("123", "0x8d592ad6DA67C3FdDe95a3cD8c33441F29C39836"))
+# 打印0x8d592ad6DA67C3FdDe95a3cD8c33441F29C39836
+print(ethereum.getHash("123"))
