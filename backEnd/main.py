7c56905c0893ba61c6fa6677b2cdeb10c7e50b4a
zzh <1497600328@qq.com>
2020-08-01 09:07:02 +0800

    添加了简易的“加入物品”接口以供测试

--- backEnd/main.py
+++ backEnd/main.py
@@ -1,4 +1,6 @@
 from flask import *
+# from ethereum.ethereum import Ethereum
+import db.ipfsdb
 app = Flask(__name__)
 @app.route('/user/<username>')
 def show_user_profile(username):
@@ -19,8 +21,37 @@ def show_subpath(subpath):
 def traceById():
     id =  request.args.get("id")
     # show the subpath after /path/
-
-    response = make_response(jsonify({'test': 'good'}),403)
+    # 实例化对象
+    ethereum = Ethereum()
+    # 初始化合约对象
+    ethereum.get_contract_instance("0x8d592ad6DA67C3FdDe95a3cD8c33441F29C39836")
+    ipfsAddress = ethereum.getHash(id)
+    Itemsdata = db.ipfsdb.get(ipfsAddress)
+    response = make_response()
     return response
+
+
+@app.route('/api/add/',methods=["POST"])
+def addById():
+    newData = json.loads(request.get_data())
+    print(newData)
+    # data = json.loads(data)
+    # productID =  request.form["productID"]
+    # productName = request.form["productName"]
+    # show the subpath after /path/
+    # 实例化对象
+    # ethereum = Ethereum()
+    # # 初始化合约对象
+    # ethereum.get_contract_instance("0x8d592ad6DA67C3FdDe95a3cD8c33441F29C39836")
+    # ipfsAddress = db.ipfsdb.add("#TODO")
+    #
+    # print(ethereum.newHash(ipfsAddress))
+    # # 建立新关系
+    # print(ethereum.newRelation("123", ipfsAddres
+    # s))
+    response = make_response(newData)
+    # print(productID)
+    return response
+
 if __name__ == "__main__":
     app.run(debug = True)
